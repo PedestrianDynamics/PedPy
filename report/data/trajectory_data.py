e66439fb81cfd56a1d0a096d7e7a7652e55dc42e
schroedtert <t.schroedter@fz-juelich.de>
2021-12-06 20:09:24 +0100

    Add first implementation of the trajectory data class

---
+++ report/data/trajectory_data.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import pathlib
+from dataclasses import dataclass
+from typing import List
+
+import pandas as pd
+from aenum import Enum, auto
+
+
+class TrajectoryUnit(Enum):
+    """Identifier of the unit of the trajectory coordinates"""
+
+    _init_ = "value __doc__"
+    METER = 1, "meter (m)"
+    CENTIMETER = 100, "centimeter (cm)"
+
+
+class TrajectoryType(Enum):
+    """Identifier for the type of trajectory"""
+
+    _init_ = "value __doc__"
+
+    PETRACK = auto(), "PeTrack trajectory"
+    JUPEDSIM = auto(), "JuPedSim trajectory"
+    FALLBACK = auto(), "trajectory of unknown type"
+
+
+@dataclass(frozen=True)
+class TrajectoryData:
+    """Trajectory Data
+
+    Wrapper around the trajectory data, holds the data as a data frame.
+
+    Note:
+        The coordinate data is stored in meter ('m')!
+
+    Attributes:
+        __data (pd.DataFrame): data frame containing the actual data in the form:
+            "ID", "frame", "X", "Y", "Z"
+
+        frame_rate (float): frame rate of the trajectory file
+
+        trajectory_type (TrajectoryType): type of the trajectory used
+
+        files (List[pothlib.Path): files from which is trajectories were read
+
+    """
+
+    _data: pd.DataFrame
+    frame_rate: float
+    trajectory_type: TrajectoryType
+    files: List[pathlib.Path]
