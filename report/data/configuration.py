d7b409b1b04285a10a1feb92ea10740b1f03ebe4
schroedtert <t.schroedter@fz-juelich.de>
2022-03-31 15:37:23 +0200

    Really use VelocityCalculator everywhere (#10)

--- report/data/configuration.py
+++ report/data/configuration.py
@@ -6,21 +6,7 @@ from typing import Dict, List
 
 from shapely.geometry import LineString, Polygon
 
-
-@dataclass(frozen=True)
-class ConfigurationVelocity:
-    """Configuration for velocity computation
-
-    Attributes:
-         frame_step (int): gives the size of time interval for calculating the velocity
-         set_movement_direction (str): indicates in which direction the velocity will be projected
-         ignore_backward_movement (bool):  indicates whether you want to ignore the movement opposite to
-                                           the direction from `set_movement_direction`
-    """
-
-    frame_step: int
-    set_movement_direction: str
-    ignore_backward_movement: bool
+from report.methods.velocity_calculator import VelocityCalculator
 
 
 @dataclass(frozen=True)
@@ -35,8 +21,8 @@ class Configuration:
         measurement_areas (Dict[int, Polygon]): measurement areas to be used in the analysis
         measurement_lines (Dict[int, LineString]): measurement line to be used in the analysis
 
-        velocity_configuration (ConfigurationVelocity): configuration for the velocity compuation
-                (see :class:ConfigurationVelocity for more detail)
+        velocity_calculator (VelocityCalculator): VelocityCalculator used for computing the
+                velocities (see :class:VelocityCalculator for more detail)
     """
 
     output_directory: pathlib.Path
@@ -46,4 +32,4 @@ class Configuration:
     measurement_areas: Dict[int, Polygon]
     measurement_lines: Dict[int, LineString]
 
-    velocity_configuration: ConfigurationVelocity
+    velocity_calculator: VelocityCalculator
