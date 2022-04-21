c0cdac8b7e199f2d0991020fd3d9beaf536e2200
schroedtert <t.schroedter@fz-juelich.de>
2022-04-21 13:27:33 +0200

    Upload package and test the install in CI

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
         "aenum >= 3.1.5",
         "numpy >= 1.21.4",
         "pandas >= 1.3.4",
-        "Shapely == 1.8.0",
+        "Shapely >= 1.8.1",
     ],
     entry_points={"console_scripts": [" jpsreport=report.application:main"]},
 )
