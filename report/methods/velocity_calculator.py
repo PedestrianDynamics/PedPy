c96b44cb53e3112cf0103ebfa301188b9daec23f
schroedtert <t.schroedter@fz-juelich.de>
2022-06-03 07:02:33 -0700

    Compute the velocity along a given movement direction (#48)

--- report/methods/velocity_calculator.py
+++ report/methods/velocity_calculator.py
@@ -1,43 +1,59 @@
+"""Module containing functions to compute velocities"""
+
 from typing import Tuple
 
+import numpy as np
 import pandas as pd
 import pygeos
 
 
 def compute_individual_velocity(
-    traj_data: pd.DataFrame, frame_rate: float, frame_step: int
+    traj_data: pd.DataFrame,
+    frame_rate: float,
+    frame_step: int,
+    movement_direction: np.ndarray = None,
 ) -> pd.DataFrame:
     """Compute the individual velocity for each pedestrian
+
+    Note: when using a movement direction the velocity may be negative!
 
     Args:
         traj_data (TrajectoryData): trajectory data
         frame_rate (float): frame rate of the trajectory
         frame_step (int): gives the size of time interval for calculating the velocity
-
+        movement_direction (np.ndarray): main movement direction on which the actual movement is
+            projected (default: None, when the un-projected movement should be used)
     Returns:
         DataFrame containing the columns 'ID', 'frame', 'speed'
     """
     df_movement = _compute_individual_movement(traj_data, frame_step)
-    df_speed = _compute_individual_speed(df_movement, frame_rate)
+    df_speed = _compute_individual_speed(df_movement, frame_rate, movement_direction)
 
     return df_speed
 
 
 def compute_mean_velocity_per_frame(
-    traj_data: pd.DataFrame, frame_rate: float, frame_step: int
+    traj_data: pd.DataFrame,
+    frame_rate: float,
+    frame_step: int,
+    movement_direction: np.ndarray = None,
 ) -> Tuple[pd.DataFrame, pd.DataFrame]:
     """Compute mean velocity per frame
+
+    Note: when using a movement direction the velocity may be negative!
 
     Args:
         traj_data (TrajectoryData): trajectory data
         frame_rate (float): frame rate of the trajectory
         frame_step (int): gives the size of time interval for calculating the velocity
+        movement_direction (np.ndarray): main movement direction on which the actual movement is
+            projected (default: None, when the un-projected movement should be used)
 
     Returns:
         DataFrame containing the columns 'frame' and 'speed' and
         DataFrame containing the columns 'ID', 'frame', 'speed' and
     """
-    df_speed = compute_individual_velocity(traj_data, frame_rate, frame_step)
+    df_speed = compute_individual_velocity(traj_data, frame_rate, frame_step, movement_direction)
     df_mean = df_speed.groupby("frame")["speed"].mean()
     df_mean = df_mean.reindex(
         list(range(traj_data.frame.min(), traj_data.frame.max() + 1)),
@@ -80,17 +96,34 @@ def _compute_individual_movement(traj_data: pd.DataFrame, frame_step: int) -> pd
     return df_movement[["ID", "frame", "start", "end", "start_frame", "end_frame"]]
 
 
-def _compute_individual_speed(movement_data: pd.DataFrame, frame_rate: float) -> pd.DataFrame:
+def _compute_individual_speed(
+    movement_data: pd.DataFrame, frame_rate: float, movement_direction=None
+) -> pd.DataFrame:
     """Compute the instantaneous velocity of each pedestrian.
 
     Args:
         movement_data (pd.DataFrame): movement data (see compute_individual_movement)
         frame_rate (float): frame rate of the trajectory data
+        movement_direction (np.ndarray): main movement direction on which the actual movement is
+            projected (default: None, when the un-projected movement should be used)
 
     Returns:
         DataFrame containing the columns: 'ID', 'frame', and 'speed' with the speed in m/s
     """
-    movement_data["distance"] = pygeos.distance(movement_data["start"], movement_data["end"])
+    if movement_direction is None:
+        movement_data["distance"] = pygeos.distance(movement_data["start"], movement_data["end"])
+    else:
+        # get the length of the projection of the movement in the frame range onto the
+        # movement_direction
+        movement_data["distance"] = (
+            np.dot(
+                pygeos.get_coordinates(movement_data["end"])
+                - pygeos.get_coordinates(movement_data["start"]),
+                movement_direction,
+            )
+            / np.linalg.norm(movement_direction)
+        )
+
     movement_data["speed"] = movement_data["distance"] / (
         (movement_data["end_frame"] - movement_data["start_frame"]) / frame_rate
     )
