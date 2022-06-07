2eae8656dad8194d9661b11a366aa06c6c4f8b1f
schroedtert <t.schroedter@fz-juelich.de>
2022-06-07 14:57:10 +0200

    Add method to compute n-t
    - move _compute_individual_movement to method_utils as it also need for the flow computation

---
+++ report/methods/flow_calculator.py
@@ -0,0 +1,74 @@
+"""Module containing functions to compute flows"""
+
+import numpy as np
+import pandas as pd
+import pygeos
+
+from report.methods.method_utils import _compute_individual_movement
+
+
+def compute_n_t(
+    traj_data: pd.DataFrame, measurement_line: pygeos.Geometry, frame_rate: float
+) -> pd.DataFrame:
+    """Compute the cumulative number of pedestrians who passed the line for each frame.
+
+    Note: for each pedestrian only the first passing of the line is considered!
+    Args:
+        traj_data (TrajectoryData): trajectory data
+        measurement_line (pygeos.Geometry): line for which n-t is computed
+        frame_rate (float): frame rate of the trajectory data
+
+    Returns:
+        DataFrame containing the columns 'frame', 'Cumulative pedestrians', and 'Time [s]'
+
+    """
+    df_movement = _compute_individual_movement(traj_data, 1, False)
+
+    # stack is used to get the coordinates in the correct order, as pygeos does not support
+    # creating linestring from points directly. The resulting array looks as follows:
+    # [[[x_0_start, y_0_start], [x_0_end, y_0_end]],
+    #  [[x_1_start, y_1_start], [x_1_end, y_1_end]], ... ]
+    df_movement["movement"] = pygeos.linestrings(
+        np.stack(
+            [
+                pygeos.get_coordinates(df_movement["start"]),
+                pygeos.get_coordinates(df_movement["end"]),
+            ],
+            axis=1,
+        )
+    )
+
+    # crossing means, the current movement crosses the line and the end point of the movement is
+    # not on the line. The result is sorted by frame number
+    crossing_frames = (
+        df_movement.loc[
+            (pygeos.intersects(df_movement["movement"], measurement_line))
+            & (~pygeos.intersects(df_movement["end"], measurement_line))
+        ]
+        .groupby("ID")["frame"]
+        .min()
+        .sort_values()
+    )
+
+    nt = (
+        crossing_frames.reset_index()
+        .groupby("frame")["frame"]
+        .size()
+        .cumsum()
+        .rename("Cumulative pedestrians")
+    )
+
+    # add missing values, to get values for each frame. First fill everything with the previous
+    # valid value (fillna('ffill')). When this is done only the frame at the beginning where no one
+    # has passed the line yet area missing (fillna(0)).
+    nt = (
+        nt.reindex(list(range(traj_data.frame.min(), traj_data.frame.max() + 1)))
+        .fillna(method="ffill")
+        .fillna(0)
+    )
+
+    nt = nt.to_frame()
+
+    # frame number is the index
+    nt["Time [s]"] = nt.index / frame_rate
+    return nt
--- report/methods/method_utils.py
+++ report/methods/method_utils.py
@@ -45,3 +45,44 @@ def get_peds_in_frame_range(
         return traj_data[traj_data["frame"].between(min_frame, max_frame, inclusive="both")]
 
     return traj_data
+
+
+def _compute_individual_movement(
+    traj_data: pd.DataFrame, frame_step: int, bidirectional: bool = True
+) -> pd.DataFrame:
+    """Compute the individual movement in the time interval frame_step
+
+    The movement is computed for the interval [frame - frame_step: frame + frame_step], if one of
+    the boundaries is not contained in the trajectory frame will be used as boundary. Hence, the
+    intervals become [frame, frame + frame_step], or [frame - frame_step, frame] respectively.
+
+    Args:
+        traj_data (pd.DataFrame): trajectory data
+        frame_step (int): how frames back and forwards are used to compute the movement
+        bidirectional (bool): if True also the prev. frame_step points will be used to determine
+            the movement
+
+    Returns:
+        DataFrame containing the columns: 'ID', 'frame', 'start', 'end', 'start_frame, and
+        'end_frame'. Where 'start'/'end' are the points where the movement start/ends, and
+        'start_frame'/'end_frame' are the corresponding frames.
+    """
+    df_movement = traj_data.copy(deep=True)
+
+    if bidirectional:
+        df_movement["start"] = (
+            df_movement.groupby("ID")["points"].shift(frame_step).fillna(df_movement["points"])
+        )
+    else:
+        df_movement["start"] = df_movement["points"]
+    df_movement["end"] = (
+        df_movement.groupby("ID")["points"].shift(-frame_step).fillna(df_movement["points"])
+    )
+    df_movement["start_frame"] = (
+        df_movement.groupby("ID")["frame"].shift(frame_step).fillna(df_movement["frame"])
+    )
+    df_movement["end_frame"] = (
+        df_movement.groupby("ID")["frame"].shift(-frame_step).fillna(df_movement["frame"])
+    )
+
+    return df_movement[["ID", "frame", "start", "end", "start_frame", "end_frame"]]
