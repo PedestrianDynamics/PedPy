Fix bug in frame range computation (#73)
* Compute frame range in area and checking if passing both entry lines
* Update usage notebook to reflect changes in code

analyzer/methods/flow_calculator.py
@@ -1,11 +1,10 @@
 """Module containing functions to compute flows"""
 from typing import Tuple
 
-import numpy as np
 import pandas as pd
 import pygeos
 
-from analyzer.methods.method_utils import _compute_individual_movement
+from analyzer.methods.method_utils import _compute_crossing_frames
 
 
 def compute_n_t(
@@ -23,41 +22,10 @@ def compute_n_t(
         DataFrame containing the columns 'frame', 'Cumulative pedestrians', and 'Time [s]' and
         DataFrame containing the columns 'ID', and 'frame'
     """
-    df_movement = _compute_individual_movement(traj_data, 1, False)
+    crossing_frames = _compute_crossing_frames(traj_data, measurement_line)
+    crossing_frames = crossing_frames.groupby("ID")["frame"].min().sort_values().reset_index()
 
-    # stack is used to get the coordinates in the correct order, as pygeos does not support
-    # creating linestring from points directly. The resulting array looks as follows:
-    # [[[x_0_start, y_0_start], [x_0_end, y_0_end]],
-    #  [[x_1_start, y_1_start], [x_1_end, y_1_end]], ... ]
-    df_movement["movement"] = pygeos.linestrings(
-        np.stack(
-            [
-                pygeos.get_coordinates(df_movement["start"]),
-                pygeos.get_coordinates(df_movement["end"]),
-            ],
-            axis=1,
-        )
-    )
-
-    # crossing means, the current movement crosses the line and the end point of the movement is
-    # not on the line. The result is sorted by frame number
-    crossing_frames = (
-        df_movement.loc[
-            (pygeos.intersects(df_movement["movement"], measurement_line))
-            & (~pygeos.intersects(df_movement["end"], measurement_line))
-        ]
-        .groupby("ID")["frame"]
-        .min()
-        .sort_values()
-    )
-
-    nt = (
-        crossing_frames.reset_index()
-        .groupby("frame")["frame"]
-        .size()
-        .cumsum()
-        .rename("Cumulative pedestrians")
-    )
+    nt = crossing_frames.groupby("frame")["frame"].size().cumsum().rename("Cumulative pedestrians")
 
     # add missing values, to get values for each frame. First fill everything with the previous
     # valid value (fillna('ffill')). When this is done only the frame at the beginning where no one
@@ -107,10 +75,10 @@ def compute_flow(
         passed_frame = nt[nt["Cumulative pedestrians"] == passed_num_peds].index.min() + 1
 
         if passed_num_peds != num_passed_before:
-            n = passed_num_peds - num_passed_before
+            num_passing_peds = passed_num_peds - num_passed_before
             t = passed_frame - passed_frame_before
 
-            flow_rate = n / t * frame_rate
+            flow_rate = num_passing_peds / t * frame_rate
             v = crossing_speeds[
                 crossing_speeds.frame.between(passed_frame_before, passed_frame, inclusive="both")
             ]["speed"].mean()