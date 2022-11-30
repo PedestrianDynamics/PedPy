5f7fe65b3184f6ab8693e357a185b921dac28f3a
schroedtert <t.schroedter@fz-juelich.de>
2022-11-30 08:30:11 +0100

    Add function to plot the trajectories

--- pedpy/plotting/plotting.py
+++ pedpy/plotting/plotting.py
@@ -5,6 +5,7 @@ import matplotlib.axes
 import matplotlib.pyplot as plt
 
 from pedpy.data.geometry import Geometry
+from pedpy.data.trajectory_data import TrajectoryData
 
 
 def plot_geometry(
@@ -58,4 +59,72 @@ def plot_geometry(
         # Paint all holes first white, then with the desired color
         ax.fill(*hole.xy, color="w", alpha=1)
         ax.fill(*hole.xy, color=hole_color, alpha=hole_alpha)
+    return ax
+
+
+def plot_trajectories(
+    *,
+    traj: TrajectoryData,
+    geometry: Optional[Geometry] = None,
+    ax: Optional[matplotlib.axes.Axes] = None,
+    **kwargs: Any,
+) -> matplotlib.axes.Axes:
+    """
+    Plot the given trajectory and geometry in 2-D
+
+    Parameters:
+        traj (TrajectoryData): Trajectory object to plot
+        geometry (Geometry, optional): Geometry object to plot
+        ax (matplotlib.axes.Axes, optional):  Axes to plot on, if None new will be created
+        traj_color (optional): color of the trajectories
+        traj_width (optional): width of the trajectories
+        traj_alpha (optional): alpha of the trajectories
+        traj_start_marker (optional): marker to indicate the start of the
+            trajectory
+        traj_end_marker (optional): marker to indicate the end of the trajectory
+        line_color (optional): color of the borders
+        line_width (optional): line width of the borders
+        walkable_area_color (optional): background color of walkable areas
+        walkable_area_alpha (optional): alpha of background color for walkable
+            areas
+        hole_color (optional): background color of holes
+        hole_alpha (optional): alpha of background color for holes
+
+    Returns:
+        matplotlib.axes.Axes instance where the geometry is plotted
+    """
+
+    traj_color = kwargs.get("traj_color", "r")
+    traj_width = kwargs.get("traj_width", 1.0)
+    traj_alpha = kwargs.get("traj_alpha", 1.0)
+
+    traj_start_marker = kwargs.get("traj_start_marker", "")
+    traj_end_marker = kwargs.get("traj_end_marker", "")
+
+    if geometry is None and ax is None:
+        ax = plt.gca()
+    elif geometry is not None:
+        ax = plot_geometry(geometry=geometry, ax=ax, **kwargs)
+
+    for id, ped in traj.data.groupby("ID"):
+        p = ax.plot(
+            ped["X"],
+            ped["Y"],
+            alpha=traj_alpha,
+            color=traj_color,
+            linewidth=traj_width,
+        )
+        ax.scatter(
+            ped[ped.frame == ped.frame.min()]["X"],
+            ped[ped.frame == ped.frame.min()]["Y"],
+            c=p[-1].get_color(),
+            marker=traj_start_marker,
+        )
+        ax.scatter(
+            ped[ped.frame == ped.frame.max()]["X"],
+            ped[ped.frame == ped.frame.max()]["Y"],
+            c=p[-1].get_color(),
+            marker=traj_end_marker,
+        )
+
     return ax
--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pedpy",
+    name="PedPy",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     author="T. Schroedter",
-    description="pedpy is a python module for pedestrian movement analysis.",
+    description="PedPy is a python module for pedestrian movement analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
@@ -19,7 +19,13 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=["pedpy", "pedpy.data", "pedpy.io", "pedpy.methods"],
+    packages=[
+        "pedpy",
+        "pedpy.data",
+        "pedpy.io",
+        "pedpy.methods",
+        "pedpy.plotting",
+    ],
     python_requires=">=3.8",
     install_requires=[
         "aenum == 3.1.5",
