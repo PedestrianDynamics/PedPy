7f8cde24d4c2a8170debe1207de2d403b4b1e65f
schroedtert <t.schroedter@fz-juelich.de>
2023-03-23 15:08:19 +0100

    Cleanup

--- tests/reference_tests/reference_test.py
+++ tests/reference_tests/reference_test.py
@@ -478,7 +478,7 @@ def test_voronoi_velocity(
     assert np.isclose(
         result[result.index.isin(reference_result.index)]["voronoi speed"],
         reference_result["voronoi speed"],
-        atol=TOLERANCE * 10,
+        atol=TOLERANCE,
     ).all()
     assert (
         result.loc[~result.index.isin(reference_result.index)].values == 0
