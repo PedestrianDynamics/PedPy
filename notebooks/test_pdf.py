ba8637723a3b974859c55af1061f616686bc9457
ThoChat <83350421+ThoChat@users.noreply.github.com>
2024-03-27 16:14:07 +0100

    2d and optimisation

--- notebooks/test_pdf.py
+++ notebooks/test_pdf.py
@@ -1,34 +1,38 @@
 import os
-import pandas as pd
-import pedpy
-import matplotlib.pyplot as plt
 import pathlib
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import pedpy
+
 
 # Set path to data directory
 path = "./notebooks/demo-data/single_file/"
 
 # Read trajectory files
-files = os.listdir(path)
+# files = os.listdir(path)
 
 # Choose data file
-file = "n34_cam2.csv"
+file = "n56_cam1.csv"
 
-# Read data and create DataFrame
+### Read data and create DataFrame
 df = pd.read_csv(os.path.join(path, file), comment="#")
+df = df.rename(columns={'ID': 'id', 'Frame': 'frame'})[["id","frame","x","y"]]
+df["frame"]
+traj = pedpy.TrajectoryData(df,25)
+
+# traj=pedpy.load_trajectory_from_ped_data_archive_hdf5(pathlib.Path(path+"00_01a.h5"))
 
 
 
-traj = pedpy.load_trajectory(
-    trajectory_file=pathlib.Path("./notebooks/demo-data/bottleneck/040_c_56_h-.txt")
-)
 
 radius_bins, pair_distibution = pedpy.compute_pair_distibution_function(
-    traj_data=traj,
-    frame_step=25,
-    radius_max=3,
-    dx=0.1)
+    traj_data=traj, radius_bin_size=0.1
+)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+fig, (ax1) = plt.subplots(1, figsize=(5, 5))
 
 # Plot g(r) on the first subplot
 ax1.plot(radius_bins, pair_distibution)
+plt.show()
--- pedpy/_version.py
+++ pedpy/_version.py
@@ -1,3 +1,3 @@
-__version__ = "1.1.0"
-__commit_hash__ = "c7d06c2"
+__version__ = "1.2.0.dev2"
+__commit_hash__ = "8c3fea1"
 
