241f1238579a9457e6d3b8901352ad852203b7fd
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2024-03-29 17:38:16 +0100

    Add documentation and vectorised version of dist calculation

--- pedpy/methods/other_characterisation_methods.py
+++ pedpy/methods/other_characterisation_methods.py
@@ -1,27 +1,41 @@
-"""Module containing functions to compute velocities."""
-from typing import Optional, Tuple
+"""Module containing functions to compute pair distribution function."""
+
+from typing import Tuple
 
 import numpy as np
 import numpy.typing as npt
 import pandas
+from scipy.spatial.distance import cdist
 
-from pedpy.column_identifier import  FRAME_COL, ID_COL,X_COL,Y_COL,V_X_COL,V_Y_COL,POINT_COL
-from pedpy.data.geometry import MeasurementArea
+from pedpy.column_identifier import FRAME_COL, ID_COL, X_COL, Y_COL
 from pedpy.data.trajectory_data import TrajectoryData
-from pedpy.methods.speed_calculator import (
-    SpeedCalculation,
-    compute_individual_speed)
 
 
 def compute_pair_distibution_function(
-    *,
-    traj_data: TrajectoryData,
-    radius_bin_size: float
-):
+    *, traj_data: TrajectoryData, radius_bin_size: float
+) -> Tuple[npt.NDArray[np.float16], npt.NDArray[np.float16]]:
+    """
+    Computes the pair distribution function g(r) for a given set of trajectory data.
 
-    df =traj_data.data
+    This function calculates the spatial distribution of positions :math:`g(r)`
+    For a variable :math:`r`, the pdf is given by the probability that two pedestrians are separated
+    by :math:`r` normalized by the probability :math:`PNI(r)` that two non-interacting pedestrians
+    are separated by :math:`r`, specifically
 
-    # Create Dataframe with all mutual distances, TTCs, and Rates of Approach
+    .. math::
+        g(r) = P(r)/PNI(r),
+
+    Args:
+    - traj_data: TrajectoryData, an object containing the trajectories.
+    - radius_bin_size: float, the size of the bins for the radial distribution function in the same units as the positions.
+
+    Returns:
+    - Tuple[np.ndarray, np.ndarray]: A tuple of two numpy arrays. The first array contains the bin edges (excluding the first bin edge),
+      and the second array contains the values of the pair distribution function :math:`g(r)` for each bin.
+    """
+    df = traj_data.data
+
+    # Create Dataframe with all mutual distances
     dist_pd_array = calculate_data_frame_pair_dist(df)
 
     # Scramble time-information to mitigate finite-size effects and calculate mutual distances of scrambled dataset
@@ -32,77 +46,45 @@ def compute_pair_distibution_function(
     radius_bins = np.arange(0, dist_pd_array.max(), radius_bin_size)
 
     # Calculate pair distibution: g(r)
-    Nb_dist=len(dist_pd_array)
+    Nb_dist = len(dist_pd_array)
     ## Actual distribution
-    pd_bins=pandas.cut(dist_pd_array,radius_bins)
-    pd_bins_normalised = (pd_bins.value_counts().sort_index().to_numpy())/Nb_dist
+    pd_bins = pandas.cut(dist_pd_array, radius_bins)
+    pd_bins_normalised = (
+        pd_bins.value_counts().sort_index().to_numpy()
+    ) / Nb_dist
     ## Scrambled distribution
-    pd_ni_bins=pandas.cut(dist_pd_ni_array,radius_bins)
-    pd_ni_bins_normalised = (pd_ni_bins.value_counts().sort_index().to_numpy())/Nb_dist
+    pd_ni_bins = pandas.cut(dist_pd_ni_array, radius_bins)
+    pd_ni_bins_normalised = (
+        pd_ni_bins.value_counts().sort_index().to_numpy()
+    ) / Nb_dist
 
-    pair_distibution = pd_bins_normalised/pd_ni_bins_normalised
+    pair_distibution = pd_bins_normalised / pd_ni_bins_normalised
 
     return radius_bins[1:], pair_distibution
 
 
-# Function to calculate Dataframe of pairwise distances
-def calculate_data_frame_pair_dist(df):
-    gdf = df.groupby(FRAME_COL)
+def calculate_data_frame_pair_dist(
+    df: pandas.DataFrame,
+) -> npt.NDArray[np.float16]:
+    distances_list = []
 
-    # Compute the number of possible combinaison of id per frame
-    N_val = 0
-    for _, df_f in gdf:
-        ids = len(df_f[ID_COL].unique())  # Number of unique ids
-        N_val += int(ids * (ids - 1) / 2)  # Calculate combinations of ids
+    for _, frame_df in df.groupby(FRAME_COL):
+        unique_ids = frame_df[ID_COL].unique()
+        N_ids = len(unique_ids)
+        if N_ids > 1:
+            coordinates = np.array(
+                frame_df[X_COL].values, frame_df[Y_COL].values
+            )
+            # Calculate pairwise distances for the current frame using cdist
+            frame_distances = cdist(
+                coordinates, coordinates, metric="euclidean"
+            )
 
-    dist_array = np.empty((N_val))  # Initialize array to store results
-    index = 0 
-    for _, df_f in gdf:  # Iterate over groups (frames)
-        gdf_f_id = df_f.groupby(ID_COL)
-        for i, (_, df_i) in enumerate(gdf_f_id):
-            for _, df_j in list(gdf_f_id)[i + 1 :]:
-                # Populate array with pairwise distances
-                dist_array[index] =(df_j[POINT_COL].iloc[0]).distance(df_i[POINT_COL].iloc[0])
-                index += 1
-    return dist_array
+            # Extract the upper triangle without the diagonal
+            distances_upper_triangle = frame_distances[
+                np.triu_indices_from(frame_distances, k=1)
+            ]
 
+            distances_list.extend(distances_upper_triangle)
 
-
-
-
-# # Function to initialize observables for a pair of data frames
-# def init_observables(observables, df_i, df_j, index):
-
-#     observables[index, 1] = ttc(
-#         df_i[X_COL].iloc[0],
-#         df_j[X_COL].iloc[0],
-#         df_i[V_X_COL].iloc[0],
-#         df_j[V_X_COL].iloc[0],
-#     )  # Calculate time-to-collision
-#     observables[index, 2] = rate_of_approach(
-#         df_i[X_COL].iloc[0],
-#         df_j[X_COL].iloc[0],
-#         df_i[V_X_COL].iloc[0],
-#         df_j[V_X_COL].iloc[0],
-#     )  # Calculate rate of approach
-
-
-## Function to calculate time-to-collision
-# def ttc(x1, x2, v1, v2, l_a=0.2, l_b=0.2):
-#     if (v1 - v2) != 0:
-#         ttc = ((x2 - x1) - (0.5 * (l_a + l_b))) / (v1 - v2)
-#         if ttc > 0:
-#             return ttc  # Formula for time-to-collision
-#         else:
-#             return 99999.9  # Return a large value if conditions are not met
-#     else:
-#         return 99999.9  # Return a large value if conditions are not met
-
-
-
-# # Function to calculate rate of approach
-# def rate_of_approach(x_a, x_b, v_a, v_b):
-#     return -1 * (v_a - v_b) * (x_a - x_b) / d(x_a, x_b)
-
-
-
+    return np.array(distances_list)
