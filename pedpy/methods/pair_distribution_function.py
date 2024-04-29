a92690c9483f9dceecf27b3c9c65d328b02959e8
ThoChat <83350421+ThoChat@users.noreply.github.com>
2024-04-29 14:55:48 +0200

    Minor correction and formating to pass all continuity tests

--- pedpy/methods/pair_distribution_function.py
+++ pedpy/methods/pair_distribution_function.py
@@ -32,24 +32,30 @@ def compute_pair_distibution_function(
 
     Args:
     - traj_data: TrajectoryData, an object containing the trajectories.
-    - radius_bin_size: float, the size of the bins for the radial distribution function in the same units as the positions.
-    - randomisation_stacking: int, Number of time the dataset will be stacked before being randomly shuffled to exact distances of non-interacting pedestrians. Larger stacking number will lead to closer approximation of true pairwise distribution of non-interacting pedestrians but with also increase computation cost.
+    - radius_bin_size: float, the size of the bins for the radial distribution function in the
+    same units as the positions.
+    - randomisation_stacking: int, Number of time the dataset will be stacked before being
+    randomly shuffled to exact distances of non-interacting pedestrians. Larger stacking number
+    will lead to closer approximation of true pairwise distribution of non-interacting pedestrians
+    but with also increase computation cost.
 
 
     Returns:
-    - Tuple[np.ndarray, np.ndarray]: A tuple of two numpy arrays. The first array contains the bin edges (excluding the first bin edge),
-      and the second array contains the values of the pair distribution function :math:`g(r)` for each bin.
+    - Tuple[np.ndarray, np.ndarray]: A tuple of two numpy arrays. The first array contains the
+    bin edges (excluding the first bin edge), and the second array contains the values of the pair-
+    distribution function :math:`g(r)` for each bin.
     """
-    df = traj_data.data
+    data_df = traj_data.data
 
     # Create Dataframe with all pairwise distances
-    pairwise_dist_array = calculate_data_frame_pair_dist(df)
+    pairwise_dist_array = calculate_data_frame_pair_dist(data_df)
 
-    # Concatenate the working dataframe df  to match the number of randomization cycles
+    # Concatenate the working dataframe (data_df) to match the number of randomization cycles
     concatenated_random_df = pandas.concat(
-        [df] * randomisation_stacking, ignore_index=True
+        [data_df] * randomisation_stacking, ignore_index=True
     )
-    # Scramble time-information to mitigate finite-size effects and calculate pairwise distances of scrambled dataset
+    # Scramble time-information to mitigate finite-size effects and calculate
+    # pairwise distances of scrambled dataset
     concatenated_random_df.frame = concatenated_random_df.frame.sample(
         frac=1
     ).reset_index(drop=True)
@@ -85,21 +91,38 @@ def compute_pair_distibution_function(
     except FloatingPointError:
         warnings.filterwarnings("default")  # reset wrnings values
         pair_distribution = pd_bins_normalised / pd_ni_bins_normalised
-        warning_message = "Random probability distribution contains null values, try using larger dx or more randomization cycles."
+        warning_message = (
+            "Random probability distribution contains null values,"
+            + "try using larger dx or more randomization cycles."
+        )
         warnings.warn(warning_message)
 
     return radius_bins[1:], pair_distribution
 
 
 def calculate_data_frame_pair_dist(
-    df: pandas.DataFrame,
+    data_df: pandas.DataFrame,
 ) -> npt.NDArray[np.float16]:
+    """
+    Calculates the pairwise distances for a given pandas DataFrame of pedestrian positions.
+
+    This function calculates the pairwise Euclidean distances between all pedestrian positions
+    and returns an array containning Euclidean distances between every possible pair of pedestrian
+    positions at every time frame.
+
+    Args:
+    - data_df: pandas.DataFrame, a DataFrame containing pedestrian positions, where columns are
+    identified by FRAME_COL, ID_COL, X_COL, and Y_COL constants.
+
+    Returns:
+    - npt.NDArray[np.float16]: A 1D numpy array of pairwise distances.
+    """
+
     distances_list = []
 
-    for _, frame_df in df.groupby(FRAME_COL):
-        unique_ids = frame_df[ID_COL].unique()
-        N_ids = len(unique_ids)
-        if N_ids > 1:
+    for _, frame_df in data_df.groupby(FRAME_COL):
+        N_unique_ids = len(frame_df[ID_COL].unique())
+        if N_unique_ids > 1:
             x_values = frame_df[X_COL].values
             y_values = frame_df[Y_COL].values
             coordinates = np.stack((x_values, y_values), axis=-1)
