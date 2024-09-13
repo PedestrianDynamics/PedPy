1bb919aeddac831f4ad4765b93f141d0da9f1b28
ThoChat <83350421+ThoChat@users.noreply.github.com>
2024-09-13 10:17:32 +0200

    Details about the computation of the pdf

--- pedpy/methods/spatial_analysis.py
+++ pedpy/methods/spatial_analysis.py
@@ -24,7 +24,8 @@ def compute_pair_distribution_function(
     """Computes the pair distribution function g(r) for a given set of trajectory data.
 
     This function calculates the spatial distribution of positions :math:`g(r)`
-    :math:`g(r)` here referred to the Euclidean distance between agents.
+    :math:`g(r)` here referred to the Euclidean distance between interacting pedestrians, i.e.,
+    pedestrians that are in the same walkable area at the same moment.
     The pdf is given by the probability that two pedestrians are separated
     by :math:`r` normalized by the probability :math:`PNI(r)` that two non-interacting pedestrians
     are separated by :math:`r`, specifically
