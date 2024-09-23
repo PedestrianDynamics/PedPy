51b1ac5e9ec8ee94da0461c1afc63956bf00986f
Christian Hirt <c.hirt@fz-juelich.de>
2024-09-23 10:02:59 +0200

    add zorder parameter to _plot_polygon and specify zorder for plot_voronoi_cells

--- pedpy/plotting/plotting.py
+++ pedpy/plotting/plotting.py
@@ -55,6 +55,7 @@ def _plot_polygon(
     line_width: float = 1,
     hole_color: Any = "lightgrey",
     hole_alpha: float = 1,
+    zorder: float = 1000,
 ) -> matplotlib.axes.Axes:
     """Plot the shapely polygon (including holes).
 
@@ -68,6 +69,8 @@ def _plot_polygon(
         line_width (float): line width of the borders
         hole_color (Any): background color of holes
         hole_alpha (float): alpha of background color for holes
+        zorder (float): Specifies the drawing order of the polygon,
+            lower values are drawn first
 
     Returns:
         matplotlib.axes.Axes instance where the polygon is plotted
@@ -84,7 +87,7 @@ def _plot_polygon(
         facecolor="none",
         linewidth=line_width,
         closed=True,
-        zorder=1000,
+        zorder=zorder,
     )
     axes.add_patch(exterior_polygon_border)
 
@@ -95,7 +98,7 @@ def _plot_polygon(
         linewidth=line_width,
         alpha=polygon_alpha,
         closed=True,
-        zorder=1000,
+        zorder=zorder,
     )
     axes.add_patch(exterior_polygon_fill)
 
@@ -109,7 +112,7 @@ def _plot_polygon(
             linewidth=line_width,
             alpha=1,
             closed=True,
-            zorder=1000,
+            zorder=zorder,
         )
         axes.add_patch(interior_polygon_border)
 
@@ -120,7 +123,7 @@ def _plot_polygon(
             linewidth=line_width,
             alpha=hole_alpha,
             closed=True,
-            zorder=1000,
+            zorder=zorder,
         )
         axes.add_patch(interior_polygon_fill)
 
@@ -1180,6 +1183,7 @@ def plot_voronoi_cells(  # noqa: PLR0912,PLR0915
             line_color=voronoi_border_color,
             polygon_color=color,
             polygon_alpha=voronoi_outside_ma_alpha,
+            zorder=1,
         )
 
         if INTERSECTION_COL in data.columns:
@@ -1191,6 +1195,7 @@ def plot_voronoi_cells(  # noqa: PLR0912,PLR0915
                     line_color="none",
                     polygon_color=color,
                     polygon_alpha=voronoi_inside_ma_alpha,
+                    zorder=1,
                 )
 
         if traj_data:
