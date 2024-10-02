284df218a3f15a7363ad8aa5f5f32bfd4cc46ca3
schroedtert <t.schroedter@posteo.de>
2024-10-02 19:42:55 +0200

    Make scripts use pre-commit-config

--- scripts/check-format.sh
+++ scripts/check-format.sh
@@ -1,19 +1,8 @@
 #! /bin/bash
 
-location="$(cd "$(dirname "${0}")";pwd -P)"
-root="$(cd "$(dirname "${location}/../..")";pwd -P)"
-
-njobs=1
-if [[ "$(uname)" == "Darwin" ]]; then
-    njobs=$(sysctl -n hw.logicalcpu)
-elif [[ "$(uname)" == "Darwin" ]]; then
-    njobs=$(nproc)
-fi
-
-
 return_code=0
 
-python -m ruff check --select I "${root}" || return_code=1
-ruff format --check "${root}" || return_code=1
+pre-commit run --hook-stage manual ruff-include-sorting-dry -a || return_code=1
+pre-commit run --hook-stage manual ruff-format-dry -a || return_code=1
 
 exit ${return_code}
