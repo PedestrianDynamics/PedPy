9c172d30a67718680248fcdafdd338adafc43f8e
schroedtert <t.schroedter@posteo.de>
2024-10-08 21:35:56 +0200

    Remove check-format script

--- scripts/check-format.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-return_code=0
-
-pre-commit run --hook-stage manual ruff-include-sorting-dry -a || return_code=1
-pre-commit run --hook-stage manual ruff-format-dry -a || return_code=1
-
-exit ${return_code}
