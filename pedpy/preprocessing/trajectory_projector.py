15d9c87c2f5d7b1febaf64e122fe3d27934775cf
unknown <ias-7170@ias7170.ias.kfa-juelich.de>
2025-12-04 16:28:30 +0100

    Added Errors for Exceptions

--- pedpy/preprocessing/trajectory_projector.py
+++ pedpy/preprocessing/trajectory_projector.py
@@ -19,10 +19,10 @@ def correct_invalid_trajectories(
         walkable_area: WalkableArea,
         back_distance_wall: float = -1 ,
         start_distance_wall: float = 0.01,
-        end_distance_wall:float = 0.03,
+        end_distance_wall:float = 0.05,
         back_distance_obst: float = -1 ,
         start_distance_obst: float = 0.01,
-        end_distance_obst:float = 0.03
+        end_distance_obst:float = 0.05
 ) -> TrajectoryData:
 
     """
@@ -92,15 +92,15 @@ def correct_invalid_trajectories(
                                      walkable_area=walkable_area)
     print("trajectory valid at beginning: ", traj_valid)
 
-    check_distance_parameters(back_distance_obst, back_distance_wall, end_distance_obst,
-                              end_distance_wall, start_distance_obst, start_distance_wall)
+    _check_distance_parameters(back_distance_obst, back_distance_wall, end_distance_obst,
+                               end_distance_wall, start_distance_obst, start_distance_wall)
 
     if not traj_valid:
 
         end_distance_all = max(end_distance_wall, end_distance_obst)
         # end_distance_wall and end_distance_obst do not necessarily have the same value.
         # In the next step, it is important to use the larger one to identify all points
-        # that may lie within the area surrounding the geometry, where corrections should
+        # that may lie within the area surrounding the geometry, where adjustments should
         # also be applied.
         walk_area_with_end_distance_all = shapely.buffer(walkable_area._polygon,
                                                         -end_distance_all)
@@ -113,7 +113,7 @@ def correct_invalid_trajectories(
               len(all_points_for_correcting))
         # to get an impression of the number of points, that need to be corrected.
 
-        geo_data = _convert_walk_Area_into_geoData(walkable_area)
+        geo_data = _convert_walk_area_into_geo_data(walkable_area)
         # walkable area is converted into a list of lists like
         # [type="obstacle"|"walls",direction,list of [p1x, p1y, p2x, p2y]]
 
@@ -138,7 +138,7 @@ def correct_invalid_trajectories(
     return trajectory_data
 
 
-def check_distance_parameters(
+def _check_distance_parameters(
 
         back_distance_obst: float,
         back_distance_wall: float,
@@ -148,17 +148,23 @@ def check_distance_parameters(
         start_distance_wall: float
 
 ):
+    """
+    The function checks, whether the distance parameters are valid and throws and InputError
+    if not.
+
+    """
+
     if back_distance_wall >= 0:
         raise InputError(
             f"back_distance_wall has to be <0, is currently {back_distance_wall}")
     if back_distance_obst >= 0:
         raise InputError(
             f"back_distance_obst has to be <0, is currently {back_distance_obst}")
-    if start_distance_wall > 0 and start_distance_wall > end_distance_wall:
+    if start_distance_wall <= 0 or start_distance_wall > end_distance_wall:
         raise InputError(
-            f"start_distance_wall and end_distance_wall have to be >= 0 and "
+            f"start_distance_wall and end_distance_wall have to be > 0 and "
             f"end_distance_wall has to be >=  start_distance_wall")
-    if start_distance_obst > 0 and start_distance_obst > end_distance_obst:
+    if start_distance_obst <= 0 or start_distance_obst > end_distance_obst:
         raise InputError(
             f"start_distance_obst and end_distance_obst have to be > 0 and "
             f"end_distance_obst has to be >=  start_distance_obst")
@@ -312,6 +318,22 @@ def _push_out_of_wall(
         x: float,
         y: float
 ) -> tuple[float, float]:
+
+    """
+
+    Args:
+        direction(float): either 1 or -1, according to the direction of the vertices of a geometry
+        geometry(list): a list of the type [type="walls",direction,list of [p1x, p1y, p2x, p2y]]
+        back_distance_wall(float):
+        end_distance_wall(float):
+        start_distance_wall(float):
+        x(float): The  x coordinate of the point to be tested and moved
+        y(float): The  y coordinate of the point to be tested and moved
+
+    Returns:
+        The moved x, y coordinates
+    """
+
     for edge in geometry[2]:
         if (_close_from_wall(x, y, edge[0], edge[1], edge[2], edge[3])):
             distance = _distance_from_wall(x, y, edge[0], edge[1], edge[2], edge[3],
@@ -331,6 +353,20 @@ def _push_out_of_obstacles(
         x: float,
         y: float,
 ) -> tuple[float, float]:
+    """
+
+    Args:
+        direction(float): either 1 or -1, according to the direction of the vertices of a geometry
+        geometry(list): a list of the type [type="walls",direction,list of [p1x, p1y, p2x, p2y]]
+        back_distance_obst(float):
+        end_distance_obst(float):
+        start_distance_obst(float):
+        x(float): The  x coordinate of the point to be tested and moved
+        y(float): The  y coordinate of the point to be tested and moved
+
+    Returns:
+        The moved x, y coordinates
+    """
 
     # moving points away from inside the walls
     for edge in geometry[2]:
@@ -355,6 +391,20 @@ def _adjust_around_corners(
         x: float,
         y: float,
 ) -> tuple[float, float]:
+    """
+
+        Args:
+            direction(float): either 1 or -1, according to the direction of the vertices of a geometry
+            geometry(list): a list of the type [type="walls",direction,list of [p1x, p1y, p2x, p2y]]
+            back_distance_obst(float):
+            end_distance_obst(float):
+            start_distance_obst(float):
+            x(float): The  x coordinate of the point to be tested and adjusted
+            y(float): The  y coordinate of the point to be tested and adjusted
+
+        Returns:
+            The moved x, y coordinates, if they had to be adjusted. Else the original x,y coordinates
+        """
 
     # gently pushing the points to avoid jumps around corners
     for edge in geometry[2]:
@@ -369,8 +419,8 @@ def _adjust_around_corners(
 
 
 def _calculating_movement_wall(
-        edge,
-        direction,
+        edges: list,
+        direction:int,
         distance: float,
         back_distance_wall: float,
         end_distance_wall: float,
@@ -378,19 +428,33 @@ def _calculating_movement_wall(
         x: float,
         y: float
 ) -> tuple[float, float]:
+    """
+    Args:
+        edges: The vertices of the edge which is too close
+        direction:either 1 or -1, according to the direction of the vertices of a geometry
+        distance: the distance between the edge and the point
+        back_distance_wall:
+        end_distance_wall:
+        start_distance_wall:
+        x(float): The  x coordinate of the point to be tested and moved
+        y(float): The  y coordinate of the point to be tested and moved
+
+    Returns:
+         The moved x, y coordinates, if they had to be adjusted. Else the original x,y coordinates
+    """
     if (back_distance_wall <= distance and distance <= end_distance_wall):
         new_distance = ((distance - back_distance_wall) * (
                 (end_distance_wall - start_distance_wall)
                 / (end_distance_wall - back_distance_wall))
                         + start_distance_wall)
-        x, y = _move_from_wall(x, y, edge[0], edge[1], edge[2], edge[3],
+        x, y = _move_from_wall(x, y, edges[0], edges[1], edges[2], edges[3],
                                new_distance, direction)
     return x, y
 
 
 def _calculating_movement_obstacle(
-        edge,
-        direction,
+        edges: list,
+        direction:int,
         distance: float,
         back_distance_obst: float,
         end_distance_obst: float,
@@ -398,14 +462,28 @@ def _calculating_movement_obstacle(
         x: float,
         y: float
 ) -> tuple[float, float]:
+    """
+       Args:
+           edges: The vertices of the edge which is too close
+           direction:either 1 or -1, according to the direction of the vertices of a geometry
+           distance: the distance between the edge and the point
+           back_distance_obst:
+           end_distance_obst:
+           start_distance_obst:
+           x(float): The  x coordinate of the point to be tested and moved
+           y(float): The  y coordinate of the point to be tested and moved
+
+       Returns:
+            The moved x, y coordinates, if they had to be adjusted. Else the original x,y coordinates
+        """
     if (back_distance_obst <= distance and distance <= end_distance_obst):
         # x' = (x-a)*((c-b)/(c-a))+b
         new_distance = ((distance - back_distance_obst) * (
                 (end_distance_obst - start_distance_obst)
                 / (end_distance_obst - back_distance_obst))
                         + start_distance_obst)
-        x, y = _move_from_wall(x, y, edge[0], edge[1], edge[2],
-                               edge[3],
+        x, y = _move_from_wall(x, y, edges[0], edges[1], edges[2],
+                               edges[3],
                                new_distance, direction)
     return x, y
 
@@ -420,6 +498,20 @@ def _calculating_movement_adjusting(
         x: float,
         y: float
 ) -> tuple[float, float]:
+    """
+       Args:
+           edges: The vertices of the edge which is too close
+           direction:either 1 or -1, according to the direction of the vertices of a geometry
+           distance: the distance between the edge and the point
+           back_distance_obst:
+           end_distance_obst:
+           start_distance_obst:
+           x(float): The  x coordinate of the point to be tested and moved
+           y(float): The  y coordinate of the point to be tested and moved
+
+       Returns:
+            The moved x, y coordinates, if they had to be adjusted. Else the original x,y coordinates
+        """
 
     if (back_distance_obst <= distance and distance <= start_distance_obst):
         # x' =((c-a)/(b-a))*(x-a)+a, a slightly different calculation to have a
@@ -552,7 +644,7 @@ def point_valid_test(
                                                           start_distance_wall, x, y)
     return x, y
 
-def _convert_walk_Area_into_geoData(
+def _convert_walk_area_into_geo_data(
         walk_area: WalkableArea
 ) -> list:
     """
@@ -592,7 +684,7 @@ def _convert_walk_Area_into_geoData(
         while i < len(obstacle) - 1:
             vertice = [float(obstacle[i][0])
                 , float(obstacle[i][1])
-                ,float (obstacle[i + 1][0])
+                , float (obstacle[i + 1][0])
                 , float(obstacle[i + 1][1])]
             vertices.append(vertice)
             i = i + 1
@@ -666,18 +758,6 @@ def _distance_from_wall(x, y, p1x, p1y, p2x, p2y, direction):
     return np.sign(sinP) * math.sqrt(math.fabs(b - xSq))
 
 
-def _new_distance_from_wall(x, y, p1x, p1y, p2x, p2y, direction):
-    P, A, B = map(np.asarray, ([x,y], [p1x,p1y], [p2x,p2y]))
-    distance=  np.linalg.norm(np.cross(P - A, B - A)) / np.linalg.norm(B - A)
-    if distance > np.linalg.norm(P - A):
-        distance = np.linalg.norm(P - A)
-    if distance > np.linalg.norm(P - B):
-        distance = np.linalg.norm(P - B)
-    return distance
-
-
-
-
 
 # return 1 if close from wall (in a circle shape), else 0
 def _close_from_wall(x, y, p1x, p1y, p2x, p2y, bias=0):
@@ -689,9 +769,6 @@ def _close_from_wall(x, y, p1x, p1y, p2x, p2y, bias=0):
     else:
         return 0
 
-
-def new_close_from_wall():
-    ...
 
 
 # return 1 if close from wall (in a triangle shape), else 0
--- tests/unit_tests/methods/test_trajectory_projector.py
+++ tests/unit_tests/methods/test_trajectory_projector.py
@@ -1,76 +1,75 @@
 import pathlib
-
+import pytest
 import numpy as np
 
+from pedpy import InputError
 from pedpy.data.geometry import WalkableArea
 from pedpy.io.trajectory_loader import load_trajectory, TrajectoryUnit
 from pedpy.methods.method_utils import is_trajectory_valid
 from pedpy.preprocessing.trajectory_projector import correct_invalid_trajectories
-from pedpy.preprocessing.trajectory_projector import _distance_from_wall, _new_distance_from_wall
 
 
-def test_correct_invalid_trajectories():
-    trajectory_data= load_trajectory(
-        trajectory_file=pathlib.Path("unit_tests/io/test-data/030_c_56_h0.txt"),
-        default_unit=TrajectoryUnit.METER)
-    walk_area = WalkableArea([ (3.5, -2), (3.5, 8), (-3.5, 8),(-3.5, -2), ],
+@pytest.fixture
+def setup_walkable_area():
+    walk_area = WalkableArea(
+        [(3.5, -2), (3.5, 8), (-3.5, 8), (-3.5, -2), ],
 
-    obstacles=[ [ (-0.7, -1.1), (-0.25, -1.1), (-0.25, -0.15), (-0.4, 0.0), (-2.8, 0.0),
-                (-2.8, 6.7), (-3.05, 6.7), (-3.05, -0.3), (-0.7, -0.3), (-0.7, -1.0), ],
+         obstacles=[[(-0.7, -1.1), (-0.25, -1.1), (-0.25, -0.15),
+                     (-0.4, 0.0), (-2.8, 0.0),
+                     (-2.8, 6.7), (-3.05, 6.7), (-3.05, -0.3),
+                     (-0.7, -0.3), (-0.7, -1.0), ],
 
-                [ (0.25, -1.1), (0.7, -1.1), (0.7, -0.3), (3.05, -0.3), (3.05, 6.7),
-                (2.8, 6.7), (2.8, 0.0), (0.4, 0.0), (0.25, -0.15), (0.25, -1.1), ],
-              ], )
+                    [(0.25, -1.1), (0.7, -1.1), (0.7, -0.3),
+                     (3.05, -0.3), (3.05, 6.7),
+                     (2.8, 6.7), (2.8, 0.0), (0.4, 0.0),
+                     (0.25, -0.15), (0.25, -1.1), ],
+                    ], )
+    return walk_area
 
-    assert (is_trajectory_valid(traj_data=trajectory_data, walkable_area=walk_area)) == False
-    corrected_trajectory_data = correct_invalid_trajectories(
-        trajectory_data=trajectory_data, walkable_area=walk_area)
-    assert is_trajectory_valid(traj_data= corrected_trajectory_data, walkable_area= walk_area) == True
-
-def test_correct_invalid_trajectories_min_paramters():
+@pytest.fixture
+def setup_trajectory_data():
     trajectory_data = load_trajectory(
         trajectory_file=pathlib.Path("unit_tests/io/test-data/030_c_56_h0.txt"),
         default_unit=TrajectoryUnit.METER)
-    walk_area = WalkableArea([(3.5, -2), (3.5, 8), (-3.5, 8), (-3.5, -2), ],
 
-    obstacles=[[(-0.7, -1.1), (-0.25, -1.1), (-0.25, -0.15), (-0.4, 0.0), (-2.8, 0.0),
-                (-2.8, 6.7), (-3.05, 6.7), (-3.05, -0.3), (-0.7, -0.3), (-0.7, -1.0), ],
+    return trajectory_data
 
-                [(0.25, -1.1), (0.7, -1.1), (0.7, -0.3), (3.05, -0.3), (3.05, 6.7),
-                (2.8, 6.7), (2.8, 0.0), (0.4, 0.0), (0.25, -0.15), (0.25, -1.1), ],
-                ], )
-
-    assert (is_trajectory_valid(traj_data=trajectory_data,
-                                walkable_area=walk_area)) == False
+def test_correct_invalid_trajectories(setup_trajectory_data, setup_walkable_area):
+    assert not  (is_trajectory_valid(traj_data=setup_trajectory_data,
+                                     walkable_area=setup_walkable_area))
     corrected_trajectory_data = correct_invalid_trajectories(
-        trajectory_data=trajectory_data, walkable_area=walk_area,
+        trajectory_data=setup_trajectory_data, walkable_area=setup_walkable_area)
+    assert is_trajectory_valid(traj_data= corrected_trajectory_data, walkable_area= setup_walkable_area) == True
+
+def test_correct_invalid_trajectories_min_paramter(setup_trajectory_data, setup_walkable_area):
+
+    assert (is_trajectory_valid(traj_data=setup_trajectory_data,
+                                walkable_area=setup_walkable_area)) == False
+    corrected_trajectory_data = correct_invalid_trajectories(
+        trajectory_data=setup_trajectory_data, walkable_area=setup_walkable_area,
         back_distance_wall=-1, start_distance_wall=0.01, end_distance_wall=0.01,
         back_distance_obst=-1, start_distance_obst=0.01, end_distance_obst=0.01 )
-    assert is_trajectory_valid(traj_data= corrected_trajectory_data, walkable_area= walk_area) == True
+    assert is_trajectory_valid(traj_data= corrected_trajectory_data, walkable_area= setup_walkable_area) == True
 
-def test_correct_invalid_trajectories_invalid_paramters():
-    trajectory_data = load_trajectory(
-        trajectory_file=pathlib.Path("unit_tests/io/test-data/030_c_56_h0.txt"),
-        default_unit=TrajectoryUnit.METER)
-    walk_area = WalkableArea([(3.5, -2), (3.5, 8), (-3.5, 8), (-3.5, -2), ],
+def test_correct_invalid_trajectories_invalid_back_distance(setup_trajectory_data, setup_walkable_area):
 
-                             obstacles=[[(-0.7, -1.1), (-0.25, -1.1), (-0.25, -0.15),
-                                         (-0.4, 0.0), (-2.8, 0.0),
-                                         (-2.8, 6.7), (-3.05, 6.7), (-3.05, -0.3),
-                                         (-0.7, -0.3), (-0.7, -1.0), ],
+    assert (is_trajectory_valid(traj_data=setup_trajectory_data,
+                                walkable_area=setup_walkable_area)) == False
 
-                                        [(0.25, -1.1), (0.7, -1.1), (0.7, -0.3),
-                                         (3.05, -0.3), (3.05, 6.7),
-                                         (2.8, 6.7), (2.8, 0.0), (0.4, 0.0),
-                                         (0.25, -0.15), (0.25, -1.1), ],
-                                        ], )
+    with pytest.raises(InputError, match = r"back_distance_wall has to be <0, is currently 0"):
+        correct_invalid_trajectories(
+            trajectory_data=setup_trajectory_data, walkable_area=setup_walkable_area,
+            back_distance_wall=0, start_distance_wall=0.01, end_distance_wall=0.04,
+            back_distance_obst=1, start_distance_obst=0.01, end_distance_obst=0.04)
 
-    assert (is_trajectory_valid(traj_data=trajectory_data,
-                                walkable_area=walk_area)) == False
-    corrected_trajectory_data = correct_invalid_trajectories(
-        trajectory_data=trajectory_data, walkable_area=walk_area,
-        back_distance_wall=-1, start_distance_wall=0.01, end_distance_wall=-0.04,
-        back_distance_obst=-1, start_distance_obst=0.01, end_distance_obst=-0.04)
-    assert is_trajectory_valid(traj_data=corrected_trajectory_data,
-                               walkable_area=walk_area) == False
-#TODO exceptions schreiben invalid paramenters
+def test_correct_invalid_trajectories_invalid_start_distance(setup_trajectory_data, setup_walkable_area):
+
+    assert (is_trajectory_valid(traj_data=setup_trajectory_data,
+                                walkable_area=setup_walkable_area)) == False
+
+    with pytest.raises(InputError, match = r"start_distance_obst and end_distance_obst have to be > 0 and "
+            "end_distance_obst has to be >=  start_distance_obst"):
+        correct_invalid_trajectories(
+            trajectory_data=setup_trajectory_data, walkable_area=setup_walkable_area,
+            back_distance_wall=-0.5, start_distance_wall=0.01, end_distance_wall=0.04,
+            back_distance_obst=-0.5, start_distance_obst=0.04, end_distance_obst=0.01)
